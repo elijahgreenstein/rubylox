d816e83c123c46a244b8ccd46749c422e97fb8e6
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-09-01 13:45:10 -0700

    Set up gem with Bundler

---
+++ lib/rubylox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubylox/version"
+
+module Rubylox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rubylox_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubylox do
+  it "has a version number" do
+    expect(Rubylox::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
