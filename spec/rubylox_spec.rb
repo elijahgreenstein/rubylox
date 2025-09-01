02b16f6a2ab17579f8713c4137158bdd7081f8ad
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-09-01 13:51:31 -0700

    Update module case (Rubylox -> RubyLox)

--- spec/rubylox_spec.rb
+++ spec/rubylox_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe Rubylox do
+RSpec.describe RubyLox do
   it "has a version number" do
-    expect(Rubylox::VERSION).not_to be nil
+    expect(RubyLox::VERSION).not_to be nil
   end
 
   it "does something useful" do
